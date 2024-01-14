f9495484b1fb11eb97f4e5c89d4bd2b579902442
Ahmed Bilal <ahmed.bilal@t-online.de>
2024-01-14 22:49:18 +0100

    fix any type errors in the code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-const Header = ({ navLinks, onNavLinkClick }) => {
+const Header = ({ navLinks, onNavLinkClick } : {navLinks: string[], onNavLinkClick: (link:string) => void}) => {
   return (
     <header className="bg-slate-700 p-10 py-4 flex justify-between items-center text-white">
       <div>
@@ -7,7 +7,7 @@ const Header = ({ navLinks, onNavLinkClick }) => {
       </div>
       <nav>
         <ul className="flex gap-10">
-          {navLinks.map((link) => (
+          {navLinks.map((link: string) => (
             <li className="nav-button" key={uuidv4()} onClick={() => onNavLinkClick(link)}>
               <button>{link}</button>
             </li>
